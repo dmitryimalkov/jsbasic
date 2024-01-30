028b0909874d05e55486df77d950a0c6dee598c1
dmitryimalkov <dmitry.i.malkov@gmail.com>
2024-01-30 10:46:29 +0300

    all

---
+++ app.js
@@ -0,0 +1,15 @@
+let name = "Max!";
+let age = 32;
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = {title: 'Developer', place: 'New York', salary: 50000};
+
+let adultYears;
+
+function calculateAdultYears () {
+    return age - 18;
+}
+
+alert(hobbies[0]);
+
+adultYears = calculateAdultYears();
+alert(adultYears);
